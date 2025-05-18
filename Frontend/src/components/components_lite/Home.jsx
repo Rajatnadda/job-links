d91d1297f9d56a5e007601e1e552ffb49bf9451b
Rajat Nadda <121797925+Rajatnadda@users.noreply.github.com>
2025-05-18 13:23:13 +0530

    Categories

--- Frontend/src/components/components_lite/Home.jsx
+++ Frontend/src/components/components_lite/Home.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Header from './Header'
+import Categories from './Categories'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
-      {/* <Categories />
-      <LatestJob />  
-      <Footer /> */}
+       <Categories />
+      {/* <LatestJob />  
+      <Footer />  */}
     </div>
   )
 }
