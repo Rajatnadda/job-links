7d59dd5007220e70c9623809aa04af8cabd7e9e4
Rajat Nadda <121797925+Rajatnadda@users.noreply.github.com>
2025-05-23 14:48:08 +0530

    complete

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173", // or "*" if testing
+  origin: "https://job-links.onrender.com", // or "*" if testing
   credentials: true
 }));
  
