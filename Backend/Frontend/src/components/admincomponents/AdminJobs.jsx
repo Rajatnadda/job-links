527733af8d798fbb3a0a2724c6665b9f826fd891
Rajat Nadda <121797925+Rajatnadda@users.noreply.github.com>
2025-06-16 11:33:14 +0530

    hi

--- Backend/Frontend/src/components/admincomponents/AdminJobs.jsx
+++ Backend/Frontend/src/components/admincomponents/AdminJobs.jsx
@@ -5,8 +5,8 @@ import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import AdminJobsTable from "./AdminJobsTable";
-import useGetAllAdminJobs from "@/hooks/useGetAllJAdminobs";
 import { setSearchJobByText } from "@/redux/jobSlice";
+import useGetAllAdminJobs from "./useGetAllAdminJobs";
 
 const AdminJobs = () => {
   useGetAllAdminJobs();
