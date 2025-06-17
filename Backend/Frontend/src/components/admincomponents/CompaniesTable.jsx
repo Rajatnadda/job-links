3faa35366e6c5de5f132fcd073f949dc789a51d7
Rajat Nadda <121797925+Rajatnadda@users.noreply.github.com>
2025-06-17 22:47:31 +0530

    hi

--- Backend/Frontend/src/components/admincomponents/CompaniesTable.jsx
+++ Backend/Frontend/src/components/admincomponents/CompaniesTable.jsx
@@ -13,6 +13,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Edit2, MoreHorizontal } from "lucide-react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import Footer from "../components_lite/Footer";
 
 const CompaniesTable = () => {
   const { companies, searchCompanyByText } = useSelector(
